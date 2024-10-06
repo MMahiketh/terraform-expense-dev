c9c42a96d69a5c18624a73089be91ae6d8835755
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-10-06 12:48:58 +0530

    Created vpc

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source = "git::https://github.com/MMahiketh/terraform-vpc-module.git?ref=master"
+
+  project        = var.project
+  environment    = var.environment
+  vpc_cidr_block = var.vpc_cidr
+  subnet_cidrs = var.subnet_cidrs
+}
