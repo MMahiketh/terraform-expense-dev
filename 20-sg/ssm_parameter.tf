303a65cc0e11e77bd413d7828dc2e84f4ba4665d
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-10-06 17:00:36 +0530

    Added sg_ids to ssm: parameter store

---
+++ 20-sg/ssm_parameter.tf
@@ -0,0 +1,29 @@
+resource "aws_ssm_parameter" "mysql_sg_id" {
+  name  = "${local.ssm_prefix}/${var.instances[0]}/sg_id"
+  type  = "String"
+  value = module.mysql.id
+}
+
+resource "aws_ssm_parameter" "backend_sg_id" {
+  name  = "${local.ssm_prefix}/${var.instances[1]}/sg_id"
+  type  = "String"
+  value = module.backend.id
+}
+
+resource "aws_ssm_parameter" "frontend_sg_id" {
+  name  = "${local.ssm_prefix}/${var.instances[2]}/sg_id"
+  type  = "String"
+  value = module.frontend.id
+}
+
+resource "aws_ssm_parameter" "ansible_sg_id" {
+  name  = "${local.ssm_prefix}/${var.other_instances[0]}/sg_id"
+  type  = "String"
+  value = module.ansible.id
+}
+
+resource "aws_ssm_parameter" "bastion_sg_id" {
+  name  = "${local.ssm_prefix}/${var.other_instances[1]}/sg_id"
+  type  = "String"
+  value = module.bastion.id
+}
