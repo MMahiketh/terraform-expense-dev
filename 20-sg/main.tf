e089258b39286605c57a1ea9637041cf9e3bd821
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-10-06 16:20:51 +0530

    Added rules to accecpt connections internet>frontend>backend>mysql

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -27,28 +27,28 @@ module "frontend" {
 
 # MySQL allow connection from backend SG
 resource "aws_security_group_rule" "mysql_backend" {
-  type = "ingress"
-  from_port = var.mysql_port
-  to_port = var.mysql_port
-  protocol = var.protocol
+  type                     = "ingress"
+  from_port                = var.mysql_port
+  to_port                  = var.mysql_port
+  protocol                 = var.protocol
   source_security_group_id = module.backend.id
-  security_group_id = module.mysql.id
+  security_group_id        = module.mysql.id
 }
 
 resource "aws_security_group_rule" "backend_frontend" {
-  type = "ingress"
-  from_port = var.api_port
-  to_port = var.api_port
-  protocol = var.protocol
+  type                     = "ingress"
+  from_port                = var.api_port
+  to_port                  = var.api_port
+  protocol                 = var.protocol
   source_security_group_id = module.frontend.id
-  security_group_id = module.backend.id
+  security_group_id        = module.backend.id
 }
 
 resource "aws_security_group_rule" "frontend_internet" {
-  type = "ingress"
-  from_port = var.http_port
-  to_port = var.http_port
-  protocol = var.protocol
-  cidr_blocks = [ "0.0.0.0/0" ]
-  security_group_id = module.mysql.id
+  type              = "ingress"
+  from_port         = var.http_port
+  to_port           = var.http_port
+  protocol          = var.protocol
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.frontend.id
 }
