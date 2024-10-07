78f19e6537f76f39bea3effa6aec8c6e4c359665
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-10-07 14:49:31 +0530

    Added ansible and bastion public ip to output

--- 40-bastion/outputs.tf
+++ 40-bastion/outputs.tf
@@ -1,3 +1,11 @@
 output "al_ami_id" {
   value = data.aws_ami.az_linux.id
 }
+
+output "ansible_public_ip" {
+  value = module.ansible.public_ip
+}
+
+output "bastion_public_ip" {
+  value = module.bastion.public_ip
+}
